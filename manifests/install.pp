b588e7ad8fc4614a1532de4f1428652fa7e1e195
mtbvang <mtb.vang@gmail.com>
2014-11-19 00:11:22 +0100

    Removed packages already declared in vagrant class.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 # == Class devpuppet::install
 #
 class devpuppet::install {
-  $packages = ['wget', 'git', 'ruby-dev', 'libxml2-dev', 'libxslt1-dev', 'g++', 'build-essential']
+  $packages = ['wget', 'git', 'ruby-dev', 'g++', 'build-essential']
 
   package { $packages: ensure => 'installed' } ->
   class { 'common::ubuntu::vagrant': }
